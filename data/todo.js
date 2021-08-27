271d002f59d7106664570af203bd761ab0e9eaab
Kothing <39289309+kothing@users.noreply.github.com>
2021-08-27 17:03:38 +0800

    Delete todo.js

--- data/todo.js
+++
@@ -1,5 +0,0 @@
-export default [
-  { text: "learn javascript", done: true, id: Date.now() },
-  { text: "learn vue", done: false, id: Date.now() + 1 },
-  { text: "learn vuex", done: false, id: Date.now() + 2 },
-];
